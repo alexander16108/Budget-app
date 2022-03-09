3a7713102c68fcbbd6ad6aa07d89bcb540c0f14f
alexander16108 <alexandermayowa05@gmail.com>
2022-03-09 14:38:57 +0100

    Generate models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :categories
+  has_many :records
+
+  validates :name, presence: true, length: { maximum: 250 }
+end
